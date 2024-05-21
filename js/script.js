b6015385ee65942bee735b04b38dd240a7cd5e29
Jack LeVasseur <159185068+Jack-LeVasseur@users.noreply.github.com>
2024-05-21 17:58:14 +0000

    Updated index.html

--- js/script.js
+++ js/script.js
@@ -3,3 +3,12 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+
+function myButtonClicked() {
+  // define numbers
+  const number = parseInt(document.getElementById("your-number").value)
+  const sum = (number / 2) * (number + 1)
+  // print answer
+  document.getElementById("answer").innerHTML = "The answer is: " + sum
+}
